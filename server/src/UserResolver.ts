383ebb2b3160e2cb3eac890cbe45874dd1bcd2b7
Juan Atehortua <juandiegox17@gmail.com>
2021-09-09 17:23:07 -0400

    Add user login and query method to retrieve all users

--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -1,5 +1,5 @@
 import { Arg, Mutation, Query, Resolver } from "type-graphql";
-import { hash } from "bcryptjs";
+import { hash, compare } from "bcryptjs";
 import { User } from "./entity/User";
 
 @Resolver()
@@ -7,6 +7,29 @@ export class UserResolver {
   @Query(() => String)
   hello() {
     return "hi!";
+  }
+
+  @Query(() => [User])
+  async users() {
+    return await User.find();
+  }
+
+  @Mutation(() => Boolean)
+  async login(@Arg("email") email: string, @Arg("password") password: string) {
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error("Could not find user");
+    }
+
+    const valid = await compare(password, user.password);
+
+    if (!valid) {
+      throw new Error("bad password");
+    }
+
+    // login successful
+    return true;
   }
 
   @Mutation(() => Boolean)
